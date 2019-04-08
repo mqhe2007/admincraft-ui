487790a2e9a59cd284d5272a6270fdf9b1854053
青衿 <122274389@qq.com>
2019-04-08 16:34:34 +0800

    修改布局名称

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import components from './components'
 import store from './store'
 import routes from './routes'
-import acDefault from './layout/Default'
-import acBlank from './layout/Blank'
+import Default from './layout/Default'
+import Blank from './layout/Blank'
 import './style/index.styl'
 export default Vue => {
   for (const key in components) {
     Vue.component(key, components[key])
   }
   Vue.prototype.$addLayout({
-    acDefault,
-    acBlank
+    Default,
+    Blank
   })
   Vue.prototype.$addStore('ui', store)
   Vue.prototype.$addRoutes(routes)
